c38b30a2ac5a4265688b464ed6b484cf768ecd15
spikers <timfengineering@gmail.com>
2017-04-07 15:18:17 -0700

    Finished promise vs sync and promise is faster? Need to test further

---
+++ sync_vs_async/script.js
@@ -0,0 +1,47 @@
+const limit = 1000000; //Million
+
+function sync() {
+  let start = getNow();
+
+  for (let i = 0; i < limit; i++) {
+    Math.random();
+  }
+
+  for (let i = 0; i < limit; i++) {
+    let sol2 = Math.random() + Math.random * 100;
+  }
+
+  let end = getNow() - start;
+  console.log('End of Sync', end);
+}
+
+function prom() {
+  let start = getNow();
+
+  let prom1 = new Promise(function (resolve, reject) {
+    for (let i = 0; i < limit; i++) {
+      Math.random();
+      resolve();
+    }
+  })
+  
+  let prom2 = new Promise(function (resolve, reject) {
+    for (let i = 0; i < limit; i++) {
+      let sol2 = Math.random() + Math.random * 100;
+      resolve();
+    }
+  })
+
+
+  Promise.all([prom1, prom2]).then(function (values) {
+    let end = getNow() - start;
+    console.log('End of Prom', end);
+  })
+}
+
+function getNow() {
+  return window.performance.now();
+}
+
+//prom();
+sync();
