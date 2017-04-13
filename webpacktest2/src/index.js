72d2175c21a977207c6083a9cb9ae76c8b55dab9
spikers <timfengineering@gmail.com>
2017-04-13 14:53:57 -0700

    Incomplete, Stable: Created a button.

--- webpacktest2/src/index.js
+++ webpacktest2/src/index.js
@@ -1,9 +1,12 @@
 var messages = require('./messages');
+import Button from './button';
 
-var newMessage = () => (`<p>message is this: ${messages.hi} heyo`);
+var newMessage = () => (Button.button);
 
 document.getElementById('app');
 app.innerHTML = newMessage();
+
+Button.attachEl();
 
 if (module.hot) {
   module.hot.accept();
